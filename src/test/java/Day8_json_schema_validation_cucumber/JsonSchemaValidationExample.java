0313c2efcd287b4ad30c68077fe4f91ff9ed492b
NataliaSozontova <50155533+NataliaSozontova@users.noreply.github.com>
2019-09-19 12:26:06 -0400

    Update JsonSchemaValidationExample.java
    Added comment about the method to be complited

--- src/test/java/Day8_json_schema_validation_cucumber/JsonSchemaValidationExample.java
+++ src/test/java/Day8_json_schema_validation_cucumber/JsonSchemaValidationExample.java
@@ -12,6 +12,7 @@ import static io.restassured.module.jsv.JsonSchemaValidator.*;
 public class JsonSchemaValidationExample {
 
     @Test
+    //to do finish code - git practice
     public void jsonSchemaValidationOfSingleSpartanAPI(){
         given().accept(ContentType.JSON)
                 .pathParam("id",97)
